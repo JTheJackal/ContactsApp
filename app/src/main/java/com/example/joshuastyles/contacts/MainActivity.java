d5ff819e4047c2ab145af5fda58b6720cfea0858
Joshua Styles <styles@outlook.com>
2018-04-02 16:29:26 +0100

    Drawer Additions

--- app/src/main/java/com/example/joshuastyles/contacts/MainActivity.java
+++ app/src/main/java/com/example/joshuastyles/contacts/MainActivity.java
@@ -80,17 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.contacts) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.addContact) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.about) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.exit) {
 
         }
 
